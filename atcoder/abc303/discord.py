69d5056f34420acc2928d4279b3496ff6633fe75
tomoya0726 <tomoya.fukui09@outlook.jp>
2023-05-30 17:44:05 +0900

    Solve discord.py

--- atcoder/abc303/discord.py
+++ atcoder/abc303/discord.py
@@ -1,5 +1,14 @@
 N, M = map(int, input().split())
 A = [[] for i in range(M)]
-for i in range(N):
+for i in range(M):
     A[i] = list(map(int, input().split()))
-print(A)
+
+S = set()
+
+for i in range(M):
+    for j in range(N-1):
+        if A[i][j] < A[i][j+1]:
+            S.add((A[i][j], A[i][j+1]))
+        else:
+            S.add((A[i][j+1], A[i][j]))
+print((N*(N-1)//2)-len(S))
