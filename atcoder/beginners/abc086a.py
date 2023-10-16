340788842895cf3d02662b730c643fbae962531d
tomoya0726 <tomoya.fukui09@outlook.jp>
2023-10-16 17:15:45 +0900

    remove

--- atcoder/beginners/abc086a.py
+++
@@ -1,8 +0,0 @@
-# 整数の入力を半角スペースで受け取る
-a, b = map(int, input().split())
-
-# 積の偶数、奇数で場合分け
-if (a * b) % 2 == 0:
-    print("Even")
-else:
-    print("Odd") 
--- atcoder/beginners/practicea.py
+++
@@ -1,8 +0,0 @@
-# 整数の入力を受け取る
-a = int(input())
-b, c = map(int, input().split())
-# 文字列の入力を受け取る
-s = input()
-
-# 出力
-print(f"{a+b+c} {s}")
